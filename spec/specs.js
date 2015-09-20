776b7bc6c465e6a6fc86296625bf19cf0ff45aae
Ian McKenney <ian.jafool@gmail.com>
2015-09-19 22:02:43 -0700

    write 3rd spec

--- spec/specs.js
+++ spec/specs.js
@@ -4,13 +4,18 @@ describe('Pizza', function() {
         expect(testPizza.type).to.equal("Cheese");
         expect(testPizza.size).to.equal("M");
     });
-});
 
-describe('Pizza', function() {
     it("creates a new Pizza with the given type, size, and toppings", function() {
         var testPizza = new Pizza("Pepperoni","L");
         testPizza.toppings.push("bacon");
         testPizza.toppings.push("olives");
         expect(testPizza.toppings).to.eql(["bacon", "olives"]);
+    });
+
+    it("calculates the cost of a pizza based on the type of pizza", function() {
+        var testPizza1 = new Pizza("Cheese", "M");
+        var testPizza2 = new Pizza("Pepperoni","L");
+        expect(testPizza1.cost).to.equal(5);
+        expect(testPizza2.cost).to.equal(5.75);
     });
 });
