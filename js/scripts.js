975cc214f71ca8086b1e5027af359ec157d16eee
Ian McKenney <ian.jafool@gmail.com>
2015-09-19 22:17:09 -0700

    pass 3rd spec

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,17 @@ function Pizza(type, size) {
     this.size = size;
     this.toppings = [];
 }
+
+Pizza.prototype.cost = function() {
+    var price = 0.0;
+    switch(this.type) {
+        case "basic":
+            price += 7.00;
+            break;
+        case "specialty":
+            price += 10.00;
+            break;
+    }
+
+    return price;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,21 +1,21 @@
 describe('Pizza', function() {
     it("creates a new Pizza with the given type and size", function() {
-        var testPizza = new Pizza("Cheese", "M");
-        expect(testPizza.type).to.equal("Cheese");
+        var testPizza = new Pizza("basic", "M");
+        expect(testPizza.type).to.equal("basic");
         expect(testPizza.size).to.equal("M");
     });
 
     it("creates a new Pizza with the given type, size, and toppings", function() {
-        var testPizza = new Pizza("Pepperoni","L");
+        var testPizza = new Pizza("specialty","L");
         testPizza.toppings.push("bacon");
         testPizza.toppings.push("olives");
         expect(testPizza.toppings).to.eql(["bacon", "olives"]);
     });
 
     it("calculates the cost of a pizza based on the type of pizza", function() {
-        var testPizza1 = new Pizza("Cheese", "M");
-        var testPizza2 = new Pizza("Pepperoni","L");
-        expect(testPizza1.cost).to.equal(5);
-        expect(testPizza2.cost).to.equal(5.75);
+        var testPizza1 = new Pizza("basic", "S");
+        var testPizza2 = new Pizza("specialty","S");
+        expect(testPizza1.cost()).to.equal(7);
+        expect(testPizza2.cost()).to.equal(10);
     });
 });
