45fdb18774f08b35f3b9548a61acaea54dd00677
Ian McKenney <ian.jafool@gmail.com>
2015-09-20 16:02:14 -0700

    add jquery to get pizza type to display when the form is submitted

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,25 @@ function Pizza(type, size) {
 Pizza.prototype.cost = function() {
     var price = 0.0;
     switch(this.type) {
-        case "basic":
+        case "Cheese":
             price += 7.00;
             break;
-        case "specialty":
+        case "Pepperoni":
+            price += 7.00;
+            break;
+        case "Butcher Block":
+            price += 10.00;
+            break;
+        case "BBQ Chicken":
+            price += 10.00;
+            break;
+        case "Real Deal":
+            price += 10.00;
+            break;
+        case "Oasis":
+            price += 10.00;
+            break;
+        case "Basil Chicken":
             price += 10.00;
             break;
     }
@@ -44,5 +59,26 @@ Pizza.prototype.cost = function() {
 // }
 
 $(document).ready(function() {
+    $("form#new-pizza").submit(function(event) {
+        event.preventDefault();
+        var imputtedType = $("select#new-type").val();
+        var imputtedSize = $("input#new-size").val();
+        var newPizza = new Pizza(imputtedType, imputtedSize);
+        $(".new-toppings").each(function() {
+            var imputtedTopping = $(this).find("input.new-toppings").val();
+            debugger;
+            newPizza.toppings.push(imputtedTopping);
+        });
 
+        $("#show-pizza").fadeOut(500);
+        $("#show-pizza").fadeIn(1000);
+        $("#show-pizza h2").text(newPizza.type);
+        $("#size").text(newPizza.size);
+
+        $("ul#toppings").text("");
+
+        newPizza.toppings.forEach(function(topping) {
+            $("ul#toppings").append("<li>" + topping + "</li>");
+        });
+    });
 });
