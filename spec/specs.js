ae6af94f0fa8ee3033f6a20179906466927c82b2
Ian McKenney <ian.jafool@gmail.com>
2015-09-19 22:20:02 -0700

    write 4th spec

--- spec/specs.js
+++ spec/specs.js
@@ -18,4 +18,11 @@ describe('Pizza', function() {
         expect(testPizza1.cost()).to.equal(7);
         expect(testPizza2.cost()).to.equal(10);
     });
+
+    it("calculates the cost of a pizza based on the size of pizza", function() {
+        var testPizza1 = new Pizza("basic", "M");
+        var testPizza2 = new Pizza("specialty","L");
+        expect(testPizza1.cost()).to.equal(14);
+        expect(testPizza2.cost()).to.equal(22);
+    });
 });
