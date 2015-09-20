9a6aeaa45d6da03d72e63d5dc1ca89d82ad604c1
Ian McKenney <ian.jafool@gmail.com>
2015-09-19 21:43:00 -0700

    write first spec

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 ## An app that calculates how much a pizza costs based on the toppings/type they choose
 
-### Hi Diane if you're reading this that means you've found a very incomplete code review.  I spent most of the day working on cover letter/linkedIn stuff with Aubrey and have just gotten started on the code review at 4:15.  I'm not worried about completing it and have no doubts that it'll be done by Tuesday at 9:45.
 
 Setup
 ----------
--- spec/specs.js
+++ spec/specs.js
@@ -1,33 +1,7 @@
-describe('wordOrder', function() {
-    it('counts one word', function() {
-        expect(wordOrder('word')).to.deep.equal({ "word": 1 });
-    });
-
-    it('counts single instances of multiple words', function() {
-        expect(wordOrder('the right answer')).to.deep.equal({ "the": 1, "right": 1, "answer": 1});
-    });
-
-    it('counts multiple instances of the same word', function() {
-        expect(wordOrder('word word')).to.deep.equal({ "word": 2 });
-    });
-
-    it('ignores casing', function() {
-        expect(wordOrder('WORD word')).to.deep.equal({ "word": 2 });
-    });
-
-    it('handles punctuation', function() {
-        expect(wordOrder('WORD! word! word....')).to.deep.equal({ "word": 3 });
-    });
-
-    it('handles floating punctuation or extra spaces', function() {
-        expect(wordOrder('WORD       WORD ... word ... brah')).to.deep.equal({ "word": 3, "brah": 1 });
-    });
-
-    it('sorts words based on how often they appear', function() {
-        expect(wordOrder('hey hey a a bear word word word')).to.deep.equal({ "word": 3, "hey": 2, "a": 2, "bear": 1 })
-    });
-
-    it('sorts', function() {
-        expect(wordOrder('bear bear bear hey hey hey hey')).to.eql({ "bear": 3, "hey": 4 });
+describe('Pizza', function() {
+    it("creates a new Pizza with the given type and size", function() {
+        var testPizza = new Pizza("Cheese", "M");
+        expect(testPizza.type) = "Cheese";
+        expect(testPizza.size) = "M";
     });
 });
