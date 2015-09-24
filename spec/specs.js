2a8b389313f49088b0371e7d1cbe3ed945e3d92c
Ian McKenney <ian.jafool@gmail.com>
2015-09-23 18:18:07 -0700

    fix specs

--- spec/specs.js
+++ spec/specs.js
@@ -13,30 +13,30 @@ describe('Pizza', function() {
     });
 
     it("calculates the cost of a pizza based on the type of pizza", function() {
-        var testPizza1 = new Pizza("basic", "S");
-        var testPizza2 = new Pizza("specialty","S");
+        var testPizza1 = new Pizza("Cheese", "S");
+        var testPizza2 = new Pizza("Butcher Block","S");
         expect(testPizza1.cost()).to.equal(7);
         expect(testPizza2.cost()).to.equal(10);
     });
 
     it("calculates the cost of a pizza based on the size of pizza", function() {
-        var testPizza1 = new Pizza("basic", "M");
-        var testPizza2 = new Pizza("specialty","L");
+        var testPizza1 = new Pizza("Cheese", "M");
+        var testPizza2 = new Pizza("Butcher Block","L");
         expect(testPizza1.cost()).to.equal(10.50);
         expect(testPizza2.cost()).to.equal(20);
     });
 
     it("calculates the cost of a pizza based on the toppings added to the pizza", function() {
-        var testPizza1 = new Pizza("basic", "M");
+        var testPizza1 = new Pizza("Cheese", "M");
         testPizza1.toppings.push("bacon");
-        var testPizza2 = new Pizza("specialty","L");
+        var testPizza2 = new Pizza("Butcher Block","L");
         testPizza2.toppings.push("olives", "feta");
         expect(testPizza1.cost()).to.equal(11.50);
         expect(testPizza2.cost()).to.equal(22);
     });
 
     it("increases the cost of the pizza if it is delivery", function() {
-        var testPizza1 = new Pizza("basic", "M");
+        var testPizza1 = new Pizza("Cheese", "M");
         testPizza1.delivery = true;
         expect(testPizza1.cost()).to.equal(13.50);
     });
